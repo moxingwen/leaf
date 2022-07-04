6a399ec8438450816a73987c4ed74e0a5c4af4a5
袁慧琪 <2640190599@qq.com>
2022-07-04 22:29:19 +0800

    对于整体项目的介绍

---
+++ README.md
@@ -0,0 +1,5 @@
+# 美团Leaf分布式ID生成系统实现
+
+### 参考技术方案
+
+https://tech.meituan.com/2017/04/21/mt-leaf.html
--- src/main/java/cn/yuan/leaf/LeafApplication.java
+++ src/main/java/cn/yuan/leaf/LeafApplication.java
@@ -3,6 +3,9 @@ package cn.yuan.leaf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 袁慧琪
+ */
 @SpringBootApplication
 public class LeafApplication {
 
